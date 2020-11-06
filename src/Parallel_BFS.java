d8ff05376151561255eb0c50ea39ac1bac889d65
Joe <joe@joewillmott.co.uk>
2020-11-06 19:06:22 +0000

    Added multi-threaded BFS.

--- src/Parallel_BFS.java
+++ src/Parallel_BFS.java
@@ -1,25 +1,32 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Parallel_BFS {
+    static final int SPLIT_CONSTANT = 10000;
 
     public static void main(String[] args) throws FileNotFoundException {
-        ArrayList<Integer>[] adj = parseInput("C:\\Users\\Joe\\Desktop\\HPC_Challenge\\src\\data_toy.txt");
+        ArrayList<Integer>[] adj = parseInput("C:\\Users\\Joe\\Desktop\\HPC_Challenge\\src\\competition_data.txt");
+
+        System.out.println("Started BFS");
 
         long start;
         long end;
 
-        start = System.nanoTime();
-        System.out.println(bfs(adj));
-        end = System.nanoTime();
+//        start = System.nanoTime();
+//        System.out.println(bfs(adj));
+//        end = System.nanoTime();
+//
+//        System.out.println(end - start);
+//
+//        start = System.nanoTime();
+//        System.out.println(bfs_2(adj));
+//        end = System.nanoTime();
 
-        System.out.println(end - start);
+//        System.out.println(end - start);
 
         start = System.nanoTime();
-        System.out.println(bfs_2(adj));
+        System.out.println(bfs_s(adj));
         end = System.nanoTime();
 
         System.out.println(end - start);
@@ -125,5 +132,100 @@ public class Parallel_BFS {
         }
 
         return answer;
+    }
+
+    static ArrayList<Integer> bfs_s(ArrayList<Integer>[] adj) {
+        List<Integer> answer = Collections.synchronizedList(new ArrayList<>());
+
+        int start = 0;
+        List<Integer> level = Collections.synchronizedList(new ArrayList<>());
+        level.add(start);
+
+        List<Integer> nextLevel;
+
+        boolean[] visited = new boolean[adj.length];
+        Arrays.fill(visited, false);
+
+        while (level.size() > 0) {
+            nextLevel = Collections.synchronizedList(new ArrayList<>());
+
+            try {
+                processLevel(adj, level, nextLevel, visited, answer);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("New Level | " + nextLevel.size());
+
+            level = nextLevel;
+        }
+
+        return new ArrayList<>(answer);
+    }
+
+    static void processLevel(ArrayList<Integer>[] adj, List<Integer> level, List<Integer> nextLevel, boolean[] visited, List<Integer> answer) throws InterruptedException {
+        if (level.size() > SPLIT_CONSTANT) {
+            List<Integer>[] partions = split(level);
+
+            Thread t1 = new Thread(() -> {
+                try {
+                    processLevel(adj, partions[0], nextLevel, visited, answer);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            t1.start();
+
+            t1.join();
+
+            Thread t2 = new Thread(() -> {
+                try {
+                    processLevel(adj, partions[1], nextLevel, visited, answer);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            t2.start();
+
+            t2.join();
+        } else {
+            for (int node : level) {
+                visited[node] = true;
+
+                if (!visited[node]) {
+                    answer.add(node);
+                }
+
+                for (int j = 0; j < adj[node].size(); j++) {
+                    int a = adj[node].get(j);
+
+                    if (!visited[a]) {
+                        nextLevel.add(a);
+                    }
+                }
+            }
+        }
+    }
+
+    static List<Integer>[] split(List<Integer> list) {
+        List<Integer> first = Collections.synchronizedList(new ArrayList<>());
+
+        List<Integer> second = Collections.synchronizedList(new ArrayList<>());
+
+        // get size of the list
+        int size = list.size();
+
+        // First size)/2 element copy into list
+        // first and rest second list
+        for (int i = 0; i < size / 2; i++)
+            first.add(list.get(i));
+
+        // Second size)/2 element copy into list
+        // first and rest second list
+        for (int i = size / 2; i < size; i++)
+            second.add(list.get(i));
+
+        // return a List of array
+        return new List[]{first, second};
     }
 }
