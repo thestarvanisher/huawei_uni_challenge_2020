c433c63b29703180881b252e5d8356dd76d87f83
Joe Willmott <33911741+joe-willmott@users.noreply.github.com>
2020-11-21 18:26:01 +0000

    Delete compile.sh

--- src/compile.sh
+++
@@ -1,5 +0,0 @@
-wget "https://www.joewillmott.co.uk/jdk-15.0.1_linux-aarch64_bin.tar.gz"
-tar -zxf jdk-15.0.1_linux-aarch64_bin.tar.gz
-export JAVA_HOME="${SCRIPTPATH=$(dirname \"$SCRIPT\")}/jdk-15.0.1"
-export export PATH=$JAVA_HOME/bin:$PATH
-javac Main.java
